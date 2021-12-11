4e8d9e7adc854156574d32f83cf1475cc24da57c
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-12-11 20:56:33 +0100

    Added widget

---
+++ Signalco.Companion.Maui/Platforms/Android/ActionWidget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+
+namespace Signalco.Companion.Maui.Platforms.Android
+{
+    [BroadcastReceiver(Label = "@string/action_widget_name")]
+    [MetaData ("android.appwidget.provider", Resource = "@xml/widget_word")]
+    internal class ActionWidget
+    {
+    }
+}
